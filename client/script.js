caa9e6f16a644cd1e38391beb882a15de9e739e5
Carmen <uo264637@uniovi.es>
2024-02-25 12:30:30 +0100

    Changed structure and added game list style

---
+++ client/script.js
@@ -0,0 +1,63 @@
+window.onload = function() {
+    loadRoomsList();
+};
+
+function loadRoomsList() {
+    fetch('http://localhost:3000/api/waitingRooms')
+        .then(response => response.json())
+        .then(data => {
+            const roomsList = document.getElementById('roomsList');
+            roomsList.innerHTML = '';
+
+            data.forEach(room => {
+                const listItem = document.createElement('li');
+                listItem.innerHTML = `${room.players.join(' vs ')} - ${room.status}`;
+                
+                const joinButton = document.createElement('button');
+                joinButton.textContent = 'Unirse';
+                joinButton.addEventListener('click', () => {
+                    toggleForm();
+                    const roomId = document.getElementById('roomId');
+                    roomId.value=room.roomId
+                });
+
+                listItem.appendChild(joinButton);
+                roomsList.appendChild(listItem);
+            });
+        })
+        .catch(error => console.error('Error al cargar la lista de juegos:', error));
+}
+
+function toggleForm() {
+    const joinForm = document.getElementById('joinForm');
+    const roomsList = document.getElementById('roomsList');
+
+    joinForm.style.display = joinForm.style.display === 'none' ? 'flex' : 'none';
+    roomsList.style.display = roomsList.style.display === 'none' ? 'block' : 'none';
+}
+
+function joinRoom(event) {
+    event.preventDefault();
+    const roomId = document.getElementById('roomId').value;
+    const playerName = document.getElementById('playerNameJoin').value;
+
+    window.location.href = `./TicTacToe/index.html?roomId=${roomId}&playerName=${playerName}`;
+}
+
+function createNewRoom(event) {
+    event.preventDefault();
+    const playerName = document.getElementById('playerNameCreate').value;
+
+    fetch('http://localhost:3000/api/createRoom', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ playerName }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        window.location.href = `./TicTacToe/index.html?roomId=${data.roomId}&playerName=${playerName}`;
+    })
+    .catch(error => console.error('Error al crear un nuevo juego:', error));
+}
---
+++ server/RoomManager.js
@@ -0,0 +1,47 @@
+class RoomManager {
+    constructor() {
+        this.rooms = {};
+        this.nextRoomId = 0;
+    }
+
+    getRoom(roomId) {
+        return this.rooms[roomId];
+    }
+
+    getWaitingRooms() {
+        const waitingRooms = Object.keys(this.rooms)
+            .filter(roomId => this.rooms[roomId].state.result.status == 'waiting')
+            .map(roomId => ({
+                roomId: roomId,
+                players: this.rooms[roomId].state.players.map(player => player.playerName),
+                status: this.rooms[roomId].state.result.status,
+            }));
+
+        return waitingRooms;
+    }
+
+    getRoomId(socketId) {
+        for (const roomId in this.rooms) {
+            const gameState = this.rooms[roomId].state;
+            const playerIndex = gameState.players.findIndex(player => player.id === socketId);
+            if (playerIndex !== -1) {
+                return roomId;
+            }
+        }
+        return -1;
+    }
+
+    createRoom(gameType) {
+        this.nextRoomId++;
+
+        this.rooms[this.nextRoomId] = {
+            gameType: gameType,
+            state: null,
+            turnTimer: 0
+        };
+
+        return this.nextRoomId;
+    }
+}
+
+module.exports = new RoomManager();
