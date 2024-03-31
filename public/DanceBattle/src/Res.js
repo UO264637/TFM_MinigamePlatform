006f46d23bdce4bd6df2cc3dd825ab946d3ba55c
Carmen <uo264637@uniovi.es>
2024-03-31 20:13:35 +0200

    Added all DanceBattle animations

--- public/DanceBattle/src/Res.js
+++ public/DanceBattle/src/Res.js
@@ -1,17 +1,25 @@
 // Lista re recursos a precargar 
 var images = {
     wheel : "res/wheel.png",
-    wheel_d : "res/wheel_d.png",
-    wheel_l : "res/wheel_l.png",
     wheel_u : "res/wheel_u.png",
     wheel_r : "res/wheel_r.png",
+    wheel_d : "res/wheel_d.png",
+    wheel_l : "res/wheel_l.png",
     wheel_m : "res/wheel_m.png",
     player_back: "res/player_back.png",
     player_back_idle: "res/player_back_idle.png",
+    player_back_up: "res/player_back_up.png",
     player_back_right: "res/player_back_right.png",
+    player_back_down: "res/player_back_down.png",
+    player_back_left: "res/player_back_left.png",
+    player_back_space: "res/player_back_space.png",
     player_front: "res/player_front.png",
-    player_front_right: "res/player_front_right.png",
     player_front_idle: "res/player_front_idle.png",
+    player_front_up: "res/player_front_up.png",
+    player_front_right: "res/player_front_right.png",
+    player_front_down: "res/player_front_down.png",
+    player_front_left: "res/player_front_left.png",
+    player_front_space: "res/player_front_space.png",
     background : "res/background.png",
     right : "res/right.png"
 };
