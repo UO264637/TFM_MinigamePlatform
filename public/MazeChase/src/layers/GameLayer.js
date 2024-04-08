5f2bffbfea415c582e5ad623b1f48166c315e35f
Carmen <uo264637@uniovi.es>
2024-04-08 17:49:20 +0200

    Changed tile size from MazeChase to 24x24

--- public/MazeChase/src/layers/GameLayer.js
+++ public/MazeChase/src/layers/GameLayer.js
@@ -129,14 +129,14 @@ class GameLayer extends Layer {
       .then((file) => {
         let lines = file.split("\n");
         let mapWidth = lines[0].length; // Ancho del mapa (número de columnas)
-        let mapOffsetX = (originalCanvasWidth - mapWidth * 20) / 2; // Offset horizontal para centrar el mapa
+        let mapOffsetX = (originalCanvasWidth - mapWidth * 24) / 2; // Offset horizontal para centrar el mapa
 
         for (let i = 0; i < lines.length; i++) {
           let line = lines[i];
           for (let j = 0; j < mapWidth; j++) {
             let symbol = line[j];
-            let x = mapOffsetX+20 + j * 20; // Calcula la posición x ajustada para centrar el mapa
-            let y = 20 + i * 20; // Calcula la posición y ajustada para centrar el mapa
+            let x = mapOffsetX+24 + j * 24; // Calcula la posición x ajustada para centrar el mapa
+            let y = 24 + i * 24; // Calcula la posición y ajustada para centrar el mapa
             this.loadMapObject(symbol, x, y);
           }
         }
