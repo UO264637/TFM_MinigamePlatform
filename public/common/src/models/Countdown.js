af8b6627a2ecbf7b31fc071677d598677126b088
Carmen Sirgo Lopez <csirgo@hiberus.com>
2024-04-11 08:39:27 +0200

    Fixed Countdown

--- public/common/src/models/Countdown.js
+++ public/common/src/models/Countdown.js
@@ -1,49 +1,46 @@
 class Countdown {
+  constructor(color = "#FFFFFF") {
+    this.value = 4;
+    this.color = color;
+    this.size = 150;
+    this.x = originalCanvasWidth * 0.5;
+    this.y = originalCanvasHeight * 0.5;
+    this.oppacity = 1;
 
-    constructor(color = "#FFFFFF") {
-        this.value = 4;
-        this.color = color;
-        this.size = 150;
-        this.x = originalCanvasWidth * 0.5;
-        this.y = originalCanvasHeight * 0.5;
+    const playNextMovement = () => {
+      console.log(this.value);
+      if (this.value == 1) {
+        this.value = "Go!";
         this.oppacity = 1;
-
-          const playNextMovement = () => {
-            if (this.value == 1) {
-                console.log("Go")
-              this.value = "Go!";
-            }
-              else if (this.value == "Go!") {
-                console.log("Go")
-                this.value = -1;
-              
-            } else {
-                this.value--;
-                this.oppacity = 1;
-                setTimeout(() => {
-                    playNextMovement();
-                }, 1000);
-              
-            }
-          };
+        setTimeout(() => {
           playNextMovement();
-    }
+        }, 1000);
+      } else if (this.value == "Go!") {
+        this.value = null;
+      } else {
+        this.value--;
+        this.oppacity = 1;
+        setTimeout(() => {
+          playNextMovement();
+        }, 1000);
+      }
+    };
+    playNextMovement();
+  }
 
-    update() {
-        this.oppacity -= 0.045;
-    }
+  update() {
+    this.oppacity -= 0.045;
+  }
 
-    paint(){
-        //console.log("A "+ console.log(this.value))
-        if (this.value != null) {
-            console.log(this.value)
-        context.font = this.size + "px RetroGaming";
-        //context.fillStyle = this.color;
-        context.fillStyle = "rgba(255, 255, 255, " + this.oppacity + ")";
-        context.textAlign = "left";
-        //context.fillText(this.value,this.x,this.y);
-        let textWidth = context.measureText(this.value).width;
-        context.fillText(this.value, this.x - textWidth/2, this.y);
-        }
+  paint() {
+    if (this.value != null) {
+      context.font = this.size + "px RetroGaming";
+      context.fillStyle = "rgba(255, 255, 255, " + this.oppacity + ")";
+      context.textAlign = "left";
+
+      let textWidth = context.measureText(this.value).width;
+
+      context.fillText(this.value, this.x - textWidth / 2, this.y);
     }
+  }
 }
