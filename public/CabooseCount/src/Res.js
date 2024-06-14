4939fe8b816a89d17a9933fce43ac3c198306b1d
Carmen <uo264637@uniovi.es>
2024-06-14 22:29:19 +0200

    Refactorized image loading code

--- public/CabooseCount/src/Res.js
+++ public/CabooseCount/src/Res.js
@@ -41,29 +41,3 @@ const images = {
   opponent_solution: "res/opponent_solution.png",
   character_background: "res/character_background.png",
 };
-
-const imageRoutes = Object.values(images);
-let allImagesLoaded = false;
-
-loadImages(0);
-
-function loadImages(index) {
-  if (index >= imageRoutes.length) {
-    if (!allImagesLoaded) {
-      allImagesLoaded = true;
-      startGame();
-    }
-    return;
-  }
-
-  const img = new Image();
-  img.src = imageRoutes[index];
-  img.onload = function () {
-    cache[imageRoutes[index]] = img;
-    loadImages(index + 1);
-  };
-  img.onerror = function () {
-    console.error(`Error loading image: ${imageRoutes[index]}`);
-    loadImages(index + 1);
-  };
-}
--- public/TicTacToe/src/Res.js
+++ public/TicTacToe/src/Res.js
@@ -28,29 +28,3 @@ let images = {
   O: "res/O.png",
   turnIndicator: "res/turn_indicator.png",
 };
-
-let imageRoutes = Object.values(images);
-let allImagesLoaded = false;
-
-loadImages(0);
-
-function loadImages(index) {
-  if (index >= imageRoutes.length) {
-    if (!allImagesLoaded) {
-      allImagesLoaded = true;
-      startGame();
-    }
-    return;
-  }
-
-  const img = new Image();
-  img.src = imageRoutes[index];
-  img.onload = function () {
-    cache[imageRoutes[index]] = img;
-    loadImages(index + 1);
-  };
-  img.onerror = function () {
-    console.error(`Error loading image: ${imageRoutes[index]}`);
-    loadImages(index + 1);
-  };
-}
